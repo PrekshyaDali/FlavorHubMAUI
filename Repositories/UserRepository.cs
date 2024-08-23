d901c29cea3532af9cfd37a1b86c8b47abc1eb79
Prekshya <prekshya.dali@bajratechnologies.com>
2024-08-23 07:55:04 +0545

    feat: implement Repo pattern and connected SQL lite to create User table

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,100 @@
+﻿using FlavorHub.Data;
+using FlavorHub.Models.SQLiteModels;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavorHub.Repositories
+{
+    public class UserRepository: Interfaces.IUserRepository
+    {
+        private readonly SQLiteAsyncConnection _Database;
+        private User _CachedUser;
+        public UserRepository(AppDbContext dbContext)
+        {
+            _Database = dbContext.GetConnection();
+        }
+
+        public void ClearCachedUser()
+        {
+            _CachedUser = null;
+        }
+
+        public async Task CreateUserAysnc(User user)
+        {
+            try
+            {
+                await _Database.InsertAsync(user);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            try
+            {
+                return await _Database.Table<User>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+        public async Task<User> GetUserByFirebaseUidAsync(string firebaseUid)
+        {
+            try
+            {
+                var user = await _Database.Table<User>().Where(u => u.FirebaseUID == firebaseUid).FirstOrDefaultAsync();
+                _CachedUser = user;
+                return user;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new NotImplementedException();
+            }
+        }
+
+        public async Task<User> GetUserByIdAsync(Guid userId)
+        {
+            try
+            {
+                var user = await _Database.Table<User>().Where(u => u.UserId == userId).FirstOrDefaultAsync();
+                _CachedUser = user;
+                return user;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new NotImplementedException();
+            }
+
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            try
+            {
+                await _Database.UpdateAsync(user);
+                // Optionally update the cached user if needed
+                _CachedUser = user;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+        }
+    }
+}
