9ad3fa8b22432b5fb32c230161de23e0e3678280
Prekshya <prekshya.dali@bajratechnologies.com>
2024-08-25 16:11:20 +0545

    feat: fetched data from recipe table and display in homepage

--- ViewModel/HomePageViewModel.cs
+++ ViewModel/HomePageViewModel.cs
@@ -2,14 +2,18 @@
 using CommunityToolkit.Mvvm.Input;
 using Firebase.Auth;
 using FlavorHub.Models.AuthModels;
+using FlavorHub.Models.SQLiteModels;
 using FlavorHub.Repositories.Interfaces;
 using FlavorHub.ViewModel;
+using FlavorHub.ViewModel.RecipeFormViewModels;
 using FlavorHub.Views.Authentication;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FlavorHub.ViewModel
 {
@@ -18,14 +22,41 @@ namespace FlavorHub.ViewModel
         private readonly FirebaseAuthClient _FirebaseAuthClient;
 
         private readonly IUserRepository _UserRepository;
+        private readonly IRecipeRepository _RecipeRepository;
+        public ICommand RefreshCommand { get; set; }
         [ObservableProperty]
         private LoginModel _LoginModel = new();
 
-        public HomePageViewModel(FirebaseAuthClient firebaseAuthClient, IUserRepository userRepository)
+        public HomePageViewModel(FirebaseAuthClient firebaseAuthClient, IUserRepository userRepository, IRecipeRepository recipeRepository)
         {
             _FirebaseAuthClient = firebaseAuthClient;
             _UserRepository = userRepository;
+            _RecipeRepository = recipeRepository;
+            RefreshCommand = new AsyncRelayCommand(RefreshRecipes);
         }
+     
+        [ObservableProperty]
+        private ObservableCollection<RecipeViewModel> _recipes;
+        public async Task LoadRecipes() {
+            try
+            {
+                var recipes = await _RecipeRepository.GetAllRecipesAsync();
+                var recipeViewModels = recipes.Select(recipe => new RecipeViewModel(recipe));
+                Recipes = new ObservableCollection<RecipeViewModel>(recipeViewModels);
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString);
+            }
+        }
+
+        [RelayCommand]
+        private async Task RefreshRecipes()
+        {
+            await LoadRecipes();
+        }
+
         [RelayCommand]
         private async Task LogOut()
         {
---
+++ ViewModel/RecipeFormViewModels/RecipeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FlavorHub.Models.SQLiteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavorHub.ViewModel.RecipeFormViewModels
+{
+    public class RecipeViewModel: ObservableObject
+    {
+        private readonly Recipe _Recipe;
+        public RecipeViewModel(Recipe recipe)
+        {
+            _Recipe = recipe;
+        }
+        public string Title => _Recipe.Title;
+
+        public string Description => _Recipe.Description;
+
+        public int CookingTime => _Recipe.CookingTime;
+
+        public string DifficultyLevel => _Recipe.DifficultyLevel;
+
+        public int Servings => _Recipe.Servings;
+
+        public string FirstImageUrl => _Recipe.ImageUrls.FirstOrDefault() ?? "dotnet_bot.png";
+    }
+}
