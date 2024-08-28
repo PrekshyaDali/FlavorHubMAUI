c72f8150f34a3dcfff513c583e909a1463265dc1
Prekshya <prekshya.dali@bajratechnologies.com>
2024-08-28 14:53:04 +0545

    feat: implement Repo pattern for comments table

---
+++ Models/SQLiteModels/Comments.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavorHub.Models.SQLiteModels
+{
+    [SQLite.Table("Comments")]
+    public class Comments
+    {
+        [PrimaryKey]
+        public Guid CommentId { get; set; } = Guid.NewGuid();
+        public Guid RecipeId { get; set; }
+        public Guid UserId { get; set; }
+
+        [MaxLength(1000)]
+        public string? CommentText { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? UserName { get; set; }
+        public string? UserProfileImage { get; set; }
+    }
+}
+
---
+++ Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavorHub.Repositories.Interfaces
+{
+    public interface ICommentsRepository
+    {
+        Task<Comments> GetCommentByIdAsync(Guid commentId);
+        Task<IEnumerable<Comments>> GetCommentsByRecipeIdAsync(Guid recipeId);
+        Task<IEnumerable<Comments>> GetCommentsByUserIdAsync(Guid userId);
+        Task AddCommentAsync(Comments comments);
+        Task UpdateCommentAsync(Comments comments);
+        Task DeleteCommentAsync(Guid commentId);
+    }
+}
